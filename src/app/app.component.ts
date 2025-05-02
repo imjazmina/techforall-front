e0fb4fdb959a674e06c7b7f89eea5d3c5fc95b4f
maria <alonsomoralesjazmin@gmail.com>
2025-05-02 00:01:39 -0300

    Creacion de modulos y estructura de las carpetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InicioComponent } from "./pages/inicio/inicio.component";
-import { NavbarComponent } from "./pages/navbar/navbar.component";
-import { FooterComponent } from "./pages/footer/footer.component";
-import { TechforallComponent } from "./pages/techforall/techforall.component";
-import { CatalogoComponent } from "./pages/catalogo/catalogo.component";
-import { DonacionComponent } from "./pages/donacion/donacion.component";
-import { RegistroComponent } from "./pages/registro/registro.component";
+import { InicioComponent } from "./features/inicio/inicio.component";
+import { NavbarComponent } from "./shared/footer/navbar/navbar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { TechforallComponent } from "./features/techforall/techforall.component";
+import { CatalogoComponent } from "./features/catalogo/catalogo.component";
+import { DonacionComponent } from "./features/donacion/donacion.component";
+import { RegistroComponent } from "./features/registro/registro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InicioComponent, NavbarComponent, FooterComponent, TechforallComponent, CatalogoComponent, DonacionComponent, RegistroComponent],
+  imports: [
+    RouterOutlet, 
+    InicioComponent, 
+    NavbarComponent, 
+    FooterComponent, 
+    TechforallComponent, 
+    CatalogoComponent, 
+    DonacionComponent, 
+    RegistroComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'techforall';
